9c64787c69a22877a302ea95051e7a072f089013
Ståle Johnsen <98233333+stalejohnsen@users.noreply.github.com>
2023-10-24 21:59:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 # Get new CIDR and TAG from IPAM API
 data "external" "ipam-reservation" {
-  program = [
-    "pwsh",
-    "${path.root}/scripts/fetchAddressPrefix.ps1",
-    var.ipam_api_guid,
-    "https://${var.ipam_app_name}.azurewebsites.net/api/spaces/${var.ipam_space}/blocks/${var.ipam_block}/reservations",
-    var.vnet_size
-  ]
+  program = ["bash", "${path.root}/scripts/new-ipam-reservation.sh"]
+  query = {
+    apiGuid   = var.ipam_api_guid
+    appName   = var.ipam_app_name
+    ipamSpace = var.ipam_space
+    ipamBlock = var.ipam_block
+    vnetSize  = var.vnet_size
+  }
 }
 
 # The landing zone module will be called once per landing_zone_*.yaml file
 # in the data directory.
 module "lz_vending" {
   source   = "Azure/lz-vending/azurerm"
-  version  = "3.1.0"
+  version  = "3.4.1"
   for_each = local.landing_zone_data_map
 
   location = each.value.location
@@ -36,11 +37,12 @@ module "lz_vending" {
   virtual_network_enabled = true
   virtual_networks = {
     vnet1 = {
-      name                    = each.value.virtual_networks.name
-      address_space           = [data.external.ipam-reservation.result.cidr] # Get from Azure IPAM 
-      resource_group_name     = each.value.virtual_networks.resource_group_name
-      hub_peering_enabled     = var.hub_peering_enabled
-      hub_network_resource_id = var.hub_network_resource_id
+      name                            = each.value.virtual_networks.name
+      address_space                   = [data.external.ipam-reservation.result.cidr] # Get from Azure IPAM 
+      resource_group_name             = each.value.virtual_networks.resource_group_name
+      hub_peering_enabled             = var.hub_peering_enabled
+      hub_peering_use_remote_gateways = false
+      hub_network_resource_id         = var.hub_network_resource_id
       tags = { # Get from Azure IPAM 
         X-IPAM-RES-ID = data.external.ipam-reservation.result.id
       }
